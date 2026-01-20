90e8f83ea592f341c8194cba1d2a25a4d4b40e26
Jeremy <jeremythegreat95@gmail.com>
2026-01-20 15:53:14 +0800

    improved UI

--- app/components/CarCard.tsx
+++ app/components/CarCard.tsx
@@ -9,8 +9,12 @@ import { FiAward } from 'react-icons/fi';
 import { PiEngine } from 'react-icons/pi';
 import { IoTicketOutline } from 'react-icons/io5';
 import { IoTrophyOutline } from 'react-icons/io5';
+import { PiMagnifyingGlassDuotone } from 'react-icons/pi';
 
 export default function CarCard({ carId, regionFlag, carName, carManufacturer, carPrice, carState, estimateDays, isNew, isRewardCar, canEngineSwap, isLotteryCar, isTrophyCar }: CarCardProps) {
+    const ICON_MD = 16;
+    const ICON_LG = 20;
+
     const indicators = [
         {
             title: 'reward car',
@@ -68,41 +72,71 @@ export default function CarCard({ carId, regionFlag, carName, carManufacturer, c
                 </div>
             }
 
-            <div className="flex justify-between">
-                <div>
-                    <div className="flex items-center gap-2">
-                        <Image
-                            src={regionFlag}
-                            width={20}
-                            height={20}
-                            alt={carName} />
+            <div>
+                <div className="flex justify-between">
+                    <div>
+                        <div className="flex items-center gap-2">
+                            <Image
+                                src={regionFlag}
+                                width={20}
+                                height={20}
+                                alt={carName} />
 
-                        <h3 className="font-medium text-sm uppercase">{carManufacturer}</h3>
+                            <h3 className="font-medium text-sm uppercase">{carManufacturer}</h3>
+                        </div>
+
+                        <div className="flex gap-4 mt-1">
+                            <h2 className="font-bold md:min-h-12">
+                                {carName}
+                            </h2>
+
+                            <div className="w-6 h-6">
+                                <PiMagnifyingGlassDuotone
+                                    size={ICON_LG}
+                                    className="cursor-pointer"
+                                    onClick={handleClick}
+                                />
+                            </div>
+                        </div>
                     </div>
 
-                    <h2
-                        className="font-bold mt-1 cursor-pointer md:min-h-12"
-                        onClick={handleClick}>
-                        {carName}
-                    </h2>
+                    {isNew && <div><h4 className="border border-amber-400 p-1 text-amber-400 text-xs rounded">New</h4></div>}
                 </div>
 
-                {isNew && <div><h4 className="border border-amber-400 p-1 text-amber-400 text-xs rounded">New</h4></div>}
+                {carData && (
+                    <div className="flex items-center justify-between gap-4 mt-2 text-sm text-gray-300">
+                        <span className="border py-1 px-3 rounded-full">
+                            <strong>PP </strong> {carData.pp}
+                        </span>
+                        <h4 className="font-bold">
+                            {carData.drivetrain} / {carData.aspiration}
+                        </h4>
+                    </div>
+                )}
             </div>
 
-            {carData && (
-                <div className="flex items-center justify-between gap-4 text-sm text-gray-300">
-                    <span className="border py-1 px-3 rounded-full">
-                        <strong>PP </strong> {carData.pp}
-                    </span>
-                    <h4 className="font-bold">
-                        {carData.drivetrain} / {carData.aspiration}
-                    </h4>
+            <div className="flex flex-col items-end mt-3">
+                <div className="min-h-4">
+                    {carState === 'limited' && <div className="flex items-center gap-2">
+                        <BsExclamationTriangle color="red" size={ICON_MD} />
+                        <h4 className="text-sm text-red-600 font-bold">Limited Stock</h4>
+                    </div>}
                 </div>
-            )}
 
-            <div className="flex justify-between items-end mt-5">
-                <div className="flex items-center gap-4">
+                <h3>
+                    <span className="text-sm">Cr.</span>
+                    <span className="text-lg md:text-xl font-bold ml-2">{carPrice}</span>
+                </h3>
+
+                <h5 className={`text-xs text-gray-400 mt-1 capitalize ${estimateDays === 1 && 'animate-pulse'}`}>
+                    {carAvailabilityTextDisplay(estimateDays)}
+                </h5>
+            </div>
+
+            <div>
+                <hr className="opacity-25" />
+
+                <div className="flex items-center gap-4 mt-3 min-h-5">
                     {indicators.map(({ title, icon: Icon, active }) => (
                         <Indicator
                             key={title}
@@ -110,30 +144,14 @@ export default function CarCard({ carId, regionFlag, carName, carManufacturer, c
                             active={active}
                             icon={
                                 <Icon
-                                    size={20}
+                                    size={ICON_LG}
                                     className={active === null ? 'opacity-25' : ''}
                                 />
                             }
                         />
                     ))}
                 </div>
-
-                <div className="flex flex-col items-end">
-                    {carState === 'limited' && <div className="flex items-center gap-2">
-                        <BsExclamationTriangle color="red" size={16} />
-                        <h4 className="text-sm text-red-600 font-bold">Limited Stock</h4>
-                    </div>}
-
-                    <h3>
-                        <span className="text-sm">Cr.</span>
-                        <span className="text-lg md:text-xl font-bold ml-2">{carPrice}</span>
-                    </h3>
-
-                    <h5 className={`text-xs text-gray-400 mt-1 capitalize ${estimateDays === 1 && 'animate-pulse'}`}>
-                        {carAvailabilityTextDisplay(estimateDays)}
-                    </h5>
-                </div>
             </div>
-        </li >
+        </li>
     )
 }
--- app/components/Indicator.tsx
+++ app/components/Indicator.tsx
@@ -2,9 +2,9 @@ import { IndicatorProps } from '../types';
 
 export default function Indicator({ icon, title, active }: IndicatorProps) {
     return (
-        <li className={`flex flex-col flex-wrap w-4 gap-2 mr-3 ${!active && 'hidden'}`}>
-            <h4 className="text-[8px] uppercase">{title}</h4>
+        <li className={`flex flex-wrap items-center gap-2 mr-2 ${!active && 'hidden'}`}>
             {icon}
+            <h4 className="text-[8px] uppercase">{title}</h4>
         </li>
     )
 }
