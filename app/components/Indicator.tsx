fc86d6127bad7aa8a8223ff7b5c9a27eb80d3c62
Jeremy <jeremythegreat95@gmail.com>
2026-01-22 10:24:42 +0800

    fix: prevent server component from being statically cached and increase indicator font size

--- app/components/Indicator.tsx
+++ app/components/Indicator.tsx
@@ -4,7 +4,7 @@ export default function Indicator({ icon, title, active }: IndicatorProps) {
     return (
         <li className={`flex flex-wrap items-center gap-2 mr-2 ${!active && 'hidden'}`}>
             {icon}
-            <h4 className="text-[8px] uppercase">{title}</h4>
+            <h4 className="text-[10px] uppercase">{title}</h4>
         </li>
     )
 }
--- app/lib/getCars.ts
+++ app/lib/getCars.ts
@@ -1,5 +1,7 @@
 export async function getCars() {
-    const response = await fetch("https://ddm999.github.io/gt7info/data.json");
+    const response = await fetch("https://ddm999.github.io/gt7info/data.json",
+        { cache: "no-store" }
+    );
 
     if (!response.ok) {
         throw new Error("Failed to fetch car list");
