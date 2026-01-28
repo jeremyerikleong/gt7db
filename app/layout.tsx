bda90c98b81a479286eac5e1cca3a5ce2796d805
Jeremy <jeremythegreat95@gmail.com>
2026-01-28 17:59:31 +0800

    style: change fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from 'next/font/google';
 import "./globals.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const montserrat = Montserrat({
+  weight: ['300', '400', '500', '700'],
+  variable: "--font-montserrat",
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
 });
 
 export const metadata: Metadata = {
@@ -27,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} antialiased`}
       >
         <Header />
         {children}
